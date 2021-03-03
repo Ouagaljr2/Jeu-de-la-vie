e814850431a6568e91c16a7216b27f8d4c357890
MAHAMAT OUAGAL <ouagal.mahamat@etu.unistra.fr>
2021-03-03 17:48:55 +0100

    erreur dans grille

--- Makefile
+++ Makefile
@@ -9,4 +9,6 @@ jeu.o:	jeu.c jeu.h grille.h
 io.o:	io.h grille.h jeu.h
 	gcc -g -Wall -c jeu.c
 clean:
-	rm *.0 main
+	rm -f *.o main
+dist:
+	tar -zcvf projet.tar.xz main.c grille.c jeu.c io.c Makefile
--- grille.c
+++ grille.c
@@ -4,41 +4,34 @@ void init_grille_from_file (char * filename, grille* g){
 	FILE * pfile = NULL;
 	pfile = fopen(filename, "r");
 	assert (pfile != NULL);
-	
+
 	int i,j,n,l,c,vivantes=0;
-	
-	fscanf(pfile, "		g->cellules=malloc(l*sizeof(int))
-", & l);
+
+	fscanf(pfile, "%d", & l);
 	fscanf(pfile, "%d", & c);
-	
+
 	alloue_grille(l,c,g){
-		int **tab2D;
-		tab2D=(int **) malloc(l*sizeof(int*));
+	    int **tab2D;
+	    tab2D=(int **) malloc(l*sizeof(int*));
 		for(int i=0;i<l;i++){
 			tab2D[i]=(int *)malloc(c*sizeof(int));
 		}
 		for(int j=0;j<l;j++){
 			for (int k=0;k<c;k++){
-				tab2D[j][k]=0:
+				tab2D[j][k]=0;
 			}
 		}
 		g->cellules=tab2D;
-	}
-	libere_grille(g){
-		for(int i=0;i<g->nbc;i++){
-			free(g->cellules[i]);	
-		}
-		free(g->cellules);
 
 	}
-	
+
 	fscanf(pfile, "%d", & vivantes);
 	for (n=0; n< vivantes; ++n){
 		fscanf(pfile, "%d", & i);
 		fscanf(pfile, "%d", & j);
 		set_vivante(i,j,*g);
 	}
-	
+
 	fclose (pfile);
 	return;
 }
@@ -47,5 +40,8 @@ void init_grille_from_file (char * filename, grille* g){
 void copie_grille (grille gs, grille gd){
 	int i, j;
 	for (i=0; i<gs.nbl; ++i) for (j=0; j<gs.nbc; ++j) gd.cellules[i][j] = gs.cellules[i][j];
-	return;	
+	return;
 }
+
+
+
